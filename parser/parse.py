a92ddd6eb0a16abfed09e0ab4fe34b30e14b9d69
JerryTeng01 <zuyuangteng@gmail.com>
2020-10-18 20:15:39 -0500

    added output, changed fields extracted

--- parser/parse.py
+++ parser/parse.py
@@ -11,16 +11,21 @@ for filename in os.listdir(os.getcwd()):
         with open(filename, 'r') as file:
             emails[filename] = {}
             mail = mailparser.parse_from_file_obj(open(filename))
-            emails[filename]['sender'] = mail.from_
+            #emails[filename]['sender'] = mail.from_
+            emails[filename]['sender'] = mail.from_[0][1]
             emails[filename]['attachments'] = mail.attachments
-            emails[filename]['to'] = mail.to
+            #emails[filename]['to'] = mail.to
+            emails[filename]['to'] = [tup[1] for tup in mail.to]
             emails[filename]['subject'] = mail.subject
             filtered = BeautifulSoup(mail.body, 'lxml').text
             emails[filename]['body'] = filtered
             b = TextBlob(filtered)
             emails[filename]['sentiment'] = b.sentiment
 
+f = open('output.txt', 'w')
 for k, v in emails.items():
-    print("\nfile:", k)
+    f.writelines(["file:" + k + "\n"])
     for key in v:
-        print(key + ":", v[key])
+        f.writelines( [key + ": " + str(v[key]) + "\n"] )
+    f.write("\n")
+f.close()
